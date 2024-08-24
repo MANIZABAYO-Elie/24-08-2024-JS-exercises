42362e5af6d3f0c5a21b5d980de1cfc5e2088962
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-24 19:20:20 +0200

    first commit

---
+++ exercise1.js
@@ -0,0 +1,15 @@
+// declaring variables 
+let a = 12;
+let b = 6;
+//multiplication
+let result1 = a*b;
+// addition 
+let result2 = a+b;
+//subtraction 
+let result3= a-b;
+// division
+let result4 =a/b;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
